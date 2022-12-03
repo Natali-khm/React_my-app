d27bbb8dbaf82ac9fb714b013f34e1bdbf20226d
Nataliya Khamitsevich <nataliyakham@gmail.com>
2022-12-03 19:33:48 +0300

    action creator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {
+  addPostActionCreator,
+  updateNewTextActionCreator,
+} from "../../redux/state";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post.jsx";
 
@@ -6,7 +10,7 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
 
   let addPost = () => {
-    props.dispatch({ type: "ADD-POST" });
+    props.dispatch(addPostActionCreator());
   };
 
   let postsElements = props.postsData.map((post) => (
@@ -19,7 +23,7 @@ const MyPosts = (props) => {
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch({ type: "UPDATE-NEW-POST-TEXT", newText: text });
+    props.dispatch(updateNewTextActionCreator(text));
   };
 
   return (
--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 const store = {
   _state: {
     profilePage: {
@@ -34,86 +37,32 @@ const store = {
   subscribe(observer) {
     this._callSubscriber = observer;
   },
-  _addPost() {
-    let newPost = {
-      id: 5,
-      message: this._state.profilePage.newPostText,
-      likesCount: 0,
-    };
-    this._state.profilePage.postsData.push(newPost);
-    this._state.profilePage.newPostText = "";
-
-    this._callSubscriber(this._state);
-  },
-  _updateNewPostText(newText) {
-    this._state.profilePage.newPostText = newText;
-    this._callSubscriber(this._state);
-  },
 
   dispatch(action) {
-    if (action.type === "ADD-POST") {
-      this._addPost();
-    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
-      this._updateNewPostText(action.newNext);
+    if (action.type === ADD_POST) {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
+      this._state.profilePage.postsData.push(newPost);
+      this._state.profilePage.newPostText = "";
+
+      this._callSubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
     }
   },
 };
 
+export const addPostActionCreator = () => ({
+  type: ADD_POST,
+});
+
+export const updateNewTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
+
 export default store;
-
-/* let renderEntireTree = () => {};
-
-let state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: "It's my first post", likesCount: 12 },
-      { id: 2, message: "How are you?", likesCount: 0 },
-    ],
-    newPostText: "it-kamasutra",
-  },
-  dialogsPage: {
-    dialogsData: [
-      { id: 1, name: "Lyana", avatar: "lyana.jpg" },
-      { id: 2, name: "Marina", avatar: "marina.png" },
-      { id: 3, name: "Tima", avatar: "tima.jpg" },
-      { id: 4, name: "Artem", avatar: "artem.png" },
-    ],
-    messagesData: [
-      { id: 1, message: "Good luck!" },
-      { id: 2, message: "See you soon" },
-      { id: 3, message: "Whats'up bro" },
-      { id: 4, message: "WTF" },
-    ],
-  },
-  sidebar: [
-    { id: 1, name: "Lyana", avatar: "lyana.jpg" },
-    { id: 2, name: "Marina", avatar: "marina.png" },
-    { id: 3, name: "Tima", avatar: "tima.jpg" },
-  ],
-};
-
-window.state = state;
-
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = "";
-
-  renderEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  renderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  renderEntireTree = observer;
-};
-
-export default state;
- */
